fd10b2f8cde09a7ad76d411128f941a224e4a4bd
ravisangwan07 <ravi00sangwan@gmail.com>
2024-07-10 17:38:25 +0530

    aveveve

---
+++ .vscode/AW-WebD/javasripte.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName("h1"){
+   
+}
---
+++ .vscode/JAVASCRIPT/class01.js
@@ -0,0 +1,14 @@
+const accountId = 144234 // javascript m (;) lgao ya na lgao wo humari marji..isse code pr koi fark nhi padta
+
+let Email = "ravi00sangwan@gmail.com"
+/*
+prefer not to use var as because of issue in block scope and fucntional scope
+and this is used to align multiline comment
+*/
+var Password = "15May2002"
+accountCity = "Bhiwani" // prefer not to use
+let accountState // if the value is not defined or declared, it will show undefined in the output;
+// accountId = 242 // not allowed as it is constant
+Email = "sangwanbidu@papa.com" // can be changed as it is a variable
+console.log(Email);
+console.table([accountId,Email, Password, accountCity, accountState])
